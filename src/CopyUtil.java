1ebfa7cfd5f4376e7eb060e32597f4a17e974f02
kirillPCHOME <kirill.007.228.74@gmail.com>
2025-02-02 21:15:54 +0300

    initial commit

---
+++ src/CopyUtil.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CopyUtil {
+    public static void copy(final InputStream src, final OutputStream dst) throws IOException {
+        // reader-to-writer byte[]-channel
+        final BlockingQueue<byte[]> buffer = new ArrayBlockingQueue<>(64);
+        // exception-channel from reader/writer threads?
+        final AtomicReference<Throwable> ex = new AtomicReference<>();
+        final ThreadGroup group = new ThreadGroup("read-write") {
+            public void uncaughtException(Thread t, Throwable e) {ex.set(e);}
+        };
+        // reader from 'src'
+        Thread reader = new Thread(group, () -> {
+            try (InputStream src0 = src) {              // 'src0' for auto-closing
+                while (true) {
+                    byte[] data = new byte[128];        // new data buffer
+                    int count = src.read(data, 1, 127); // read up to 127 bytes
+                    data[0] = (byte) count;             // 0-byte is length-field
+                    buffer.put(data);                   // send to writer
+                    if (count == -1) {break;}           // src empty
+                }
+            } catch (Exception e) {group.interrupt();}  // interrupt writer
+        });
+        reader.start();
+        // writer to 'dst'
+        Thread writer = new Thread(group, () -> {
+            try (OutputStream dst0 = dst) {      // 'dst0' for auto-closing
+                while (true) {
+                    byte[] data = buffer.take(); // get new data from reader
+                    if (data[0] == -1) {break;}  // its last data
+                    dst.write(data, 1, data[0]); // 
+                }
+            } catch (Exception e) {group.interrupt();}  // interrupt writer
+        });
+        writer.start();
+        // wait to complete read/write operations
+        try {
+            reader.join(); // wait for reader
+            writer.join(); // wait for writer
+        } catch (InterruptedException e) {throw new IOException(e);}
+        if (ex.get() != null) {throw new IOException(ex.get());}
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,20 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Test {
-
-
+    public static void main(String[] args) throws IOException {
+        Random rnd = new Random(0);
+        byte[] testData = new byte[64 * 1024];
+        rnd.nextBytes(testData);
+        ByteArrayOutputStream dst = new ByteArrayOutputStream();
+        CopyUtil.copy(new ByteArrayInputStream(testData), dst);
+        if (!Arrays.equals(testData, dst.toByteArray())) {
+            throw new AssertionError("Lab decision wrong!");
+        } else {
+            System.out.println("OK!");
+        }
+    }
 }
